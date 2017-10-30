f1f1bc23b9121e61fadbd6c2f759f2d856966229
shybyte <ein@volloeko.de>
2017-10-30 08:20:12 +0100

    Improved binary-search solution

--- binary-search/src/lib.rs
+++ binary-search/src/lib.rs
@@ -3,12 +3,13 @@ use std::cmp::Ordering;
 pub fn find<T: Ord, C: AsRef<[T]>>(elements: C, needle: T) -> Option<usize>
 {
     let slice: &[T] = elements.as_ref();
-    let (head, middle_and_tail) = elements.as_ref().split_at(slice.len() / 2);
+    let (head, middle_and_tail) = slice.split_at(slice.len() / 2);
     middle_and_tail.first().and_then(|middle_element|
         match needle.cmp(middle_element) {
             Ordering::Less => find(head, needle),
             Ordering::Equal => Some(head.len()),
-            Ordering::Greater => find(&middle_and_tail[1..], needle).map(|i| i + head.len() + 1)
+            Ordering::Greater => find(&middle_and_tail[1..], needle)
+                .map(|i| i + head.len() + 1)
         }
     )
 }
